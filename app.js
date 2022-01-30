51ba964d8236b7a6e6faba95b6870d86057275d3
Nate <natesvoboda64@gmail.com>
2022-01-30 13:50:02 -0500

    adding initial node prompts for questions

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+            }
+        ]);
+    };
+    
+    promptUser().then(answers => console.log(answers));
+
